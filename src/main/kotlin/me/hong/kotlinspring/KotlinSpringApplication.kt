e034f2e7b60b408896a2fbdf6b24efa32367e4d0
Hong <hkhong91@gmail.com>
2020-09-11 18:30:07 +0900

    add pid listener

--- src/main/kotlin/me/hong/kotlinspring/KotlinSpringApplication.kt
+++ src/main/kotlin/me/hong/kotlinspring/KotlinSpringApplication.kt
@@ -1,11 +1,14 @@
 package me.hong.kotlinspring
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.boot.context.ApplicationPidFileWriter
 
 @SpringBootApplication
 class KotlinSpringApplication
 
 fun main(args: Array<String>) {
-  runApplication<KotlinSpringApplication>(*args)
+  val application = SpringApplication(KotlinSpringApplication::class.java)
+  application.addListeners(ApplicationPidFileWriter())
+  application.run(*args)
 }
